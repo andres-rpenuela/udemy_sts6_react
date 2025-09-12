f5cf13e8fada9fc34ebaf9f8fa7e6a8b37da05ea
Andrés Ruiz Peñuela <andresruizpenuela@gmail.com>
2025-09-12 17:20:20 +0200

    [sts6] inicializar la base de datos y arreglar configuracion de docker

--- backend-products/src/main/java/com/codearp/application/repositories/ProductRepository.java
+++ backend-products/src/main/java/com/codearp/application/repositories/ProductRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 
 // Paradigma HATEOAS "Hypermedia As The Engine Of Application State" - Para spring rest data
-@RepositoryRestController(path = "products") // Propcionar un servicio crud automaticamente 
+@RepositoryRestController(path = "products") // Propcionar un servicio crud automaticamente
 public interface ProductRepository extends CrudRepository<Product,Long> {
 }
---
+++ backend-products/src/main/resources/data-test.sql
@@ -0,0 +1,6 @@
+INSERT INTO products (name, description, price, quantity, version)
+VALUES
+    ('Laptop', 'Laptop gamer de 16GB RAM', 1200.50, 10, 0),
+    ('Mouse', 'Mouse inalámbrico Logitech', 25.99, 50, 0),
+    ('Teclado', 'Teclado mecánico retroiluminado', 79.99, 30, 0),
+    ('Monitor', 'Monitor 27 pulgadas 4K', 300.00, 15, 0);
